1dfc31fd71ba78d32b460369f3b84d2f9b83d03a
paradox420power <yodapowr@gmail.com>
2019-03-15 11:50:03 -0700

    Add files via upload
    Added automatic redirects when timer runs out, variation in timer size, reworked timer output, & added field for user to indicate what url they want to go to on timeout. Method was added to make sure url has http:// & a domain, but if there is a typo it will still redirect to 404.

--- MVP.js
+++ MVP.js
@@ -1,5 +1,6 @@
 
 var intervalQueue = [];
+var userWin;
 
 window.onload = function init(){
 	var hours = 0;
@@ -8,25 +9,34 @@ window.onload = function init(){
 	
 	document.getElementById("TimerOutput").innerHTML = hours + "h " + minutes + "m " + seconds + "s ";
 	
+	document
+	
 	document.getElementById("addInterval").onclick = function(){
 		addInterval();
 	};
 	document.getElementById("startTime").onclick = function(){
+		var startURL = document.getElementById("nextURL").value;
+		userWin = window.open(makeValidURL(startURL), '_blank');
 		startTimer();
 	};
 }
 
 function addInterval(){
-	var hours, minutes, seconds;
+	var hours, minutes, seconds, redirects, nextURL;
 	hours = document.getElementById("hourField").value;
 	minutes = document.getElementById("minuteField").value;
 	seconds = document.getElementById("secondField").value;
+	redirects = document.getElementById("urlChange").checked;
+	console.log(redirects);
+	nextURL = document.getElementById("nextURL").value;
 	
 	if(hours != 0 || minutes != 0 || seconds != 0){ //push non-empty
 		let nextInterval = {
 			hour: hours,
 			minute: minutes,
-			second: seconds
+			second: seconds,
+			redirect: redirects,
+			url: nextURL
 		};
 		intervalQueue.push(nextInterval);
 		writeQueue();
@@ -39,9 +49,39 @@ function addInterval(){
 function writeQueue(){
 	var toWrite = "";
 	for(let x = 0; x < intervalQueue.length; x++){
-		toWrite += intervalQueue[x].hour + "h " + intervalQueue[x].minute + "m " + intervalQueue[x].second + "s<br>";
+		toWrite += "Timer: " + intervalQueue[x].hour + "h " + intervalQueue[x].minute + "m " + intervalQueue[x].second + "s<br>";
+		if(intervalQueue[x].redirect === true)
+			toWrite += "Sending user to \"" + intervalQueue[x].url + "\"<br>";
+		toWrite += "<br>";
 	}
 	document.getElementById("queue").innerHTML = toWrite;
+}
+
+//not all users are going to enter http://www. OR .com/.net/.org
+//to their website address, we need to make sure it is there
+function makeValidURL(currentURL){
+	var index = 0;
+	var httpHead = "http://";
+	var httpsHead = "https://";
+	var domains = [".com", ".net", ".org", ".edu"];
+	//check the correct headers are attached
+	var toCheck = currentURL.substr(0,7); //does it have http?
+	if(toCheck !== httpHead){
+		toCheck = currentURL.substr(0,8); //does it have https?
+		if(toCheck !== httpsHead){
+			currentURL = httpHead + currentURL; //has niether, add http://
+		}
+	}
+	var hasDomain = false;
+	for(var x = 0; x < domains.length; x++){
+		if(currentURL.includes(domains[x]))
+			hasDomain = true;
+	}
+	if(!hasDomain){
+		currentURL += ".com";
+	}	
+	
+	return currentURL;
 }
 
 function startTimer(){
@@ -90,10 +130,16 @@ function startTimer(){
 		// If the count down is finished, write some text
 		if (timeUp) {
 			if(intervalQueue.length > 0){
+				//update timer
 				hoursLeft = intervalQueue[0].hour;
 				minutesLeft = intervalQueue[0].minute;
 				secondsLeft = intervalQueue[0].second;
-				output = hoursLeft + "h " + minutesLeft + "m " + secondsLeft + "s "
+				output = hoursLeft + "h " + minutesLeft + "m " + secondsLeft + "s ";
+				//check if the url should be changed
+				if(intervalQueue[0].redirect === true){
+					var jumpTo = makeValidURL(intervalQueue[0].url) + "";
+					userWin.location.href = jumpTo;
+				}
 				document.getElementById("TimerOutput").innerHTML = output;
 				document.title = output;
 				timeUp = false;
@@ -102,6 +148,7 @@ function startTimer(){
 			}else{
 				clearInterval(x);
 				document.getElementById("TimerOutput").innerHTML = "EXPIRED";
+				userWin.close();
 			}
 		}
 	}, 1000);
